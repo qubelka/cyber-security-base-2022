8497e4e220327ac1056b3e59ad8ee5afb0c63f2f
Qubelka <42945433+qubelka@users.noreply.github.com>
2022-07-20 08:56:59 +0200

    Add sql commands for inserting books

---
+++ bookstore/books.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO books_book (id, title, author, published, slug, total, sold)
+VALUES (1, "Crafting Interpreters", "Robert Nystrom", 2021, "crafting-interpreters", 50, 0);
+
+INSERT INTO books_book (id, title, author, published, slug, total, sold)
+VALUES (2, "Language Implementation Patterns", "Terence Parr", 2010, "language-implementation-patterns", 50, 0);
+
+INSERT INTO books_book (id, title, author, published, slug, total, sold)
+VALUES (3, "Engineering a Compiler", "Keith Cooper", 2011, "engineering-a-compiler", 50, 0);
